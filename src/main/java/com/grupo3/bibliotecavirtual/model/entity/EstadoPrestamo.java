6680d9087201b332a6590de6fac6e2ccd1449878
Estefania-Catano <ecatano1@cesde.net>
2026-02-26 20:05:06 -0500

    feat: crear entidades Perfil y Prestamo y agregar enum EstadoPrestamo

---
+++ src/main/java/com/grupo3/bibliotecavirtual/model/entity/EstadoPrestamo.java
@@ -0,0 +1,6 @@
+package com.grupo3.bibliotecavirtual.model.entity;
+
+public class EstadoPrestamo {
+    Devuelto,
+    Prestado
+}
--- src/main/java/com/grupo3/bibliotecavirtual/model/entity/Perfil.java
+++ src/main/java/com/grupo3/bibliotecavirtual/model/entity/Perfil.java
@@ -1,25 +1,38 @@
-package com.grupo3.bibliotecavirtual.model.entity;
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "perfil")
-@Getter @Setter
+@Getter
+@Setter
+
 public class Perfil {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String bio;
+  private String tipoDocumento;
+  private String numeroDocumento;
+  private String nombre;
+  private String apellido;
+  private String direccion;
+  private String telefono;
 
-    private LocalDate fechaNacimiento;
+  @OneToOne
+    @JoinColumn(name = "usuario_id", unique = true
+        private Usuario usuario;
+    )
 
-    private String telefono;
+    @OneToMany(mappedBy = "perfil")
+private List<Prestamo> prestamos;
 
-
-    @OneToOne(mappedBy = "perfil") // Nombre del atributo en Usuario
-    private User usuario;
 }
