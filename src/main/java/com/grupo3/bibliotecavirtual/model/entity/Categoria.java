223f7f2854aa22f06a2a88a383c2e04353df4bda
lorenamejiag <loregalvan0930@gmail.com>
2026-02-28 17:00:53 -0500

    Se crea entidad Categoria y relación con Libro

---
+++ src/main/java/com/grupo3/bibliotecavirtual/model/entity/Categoria.java
@@ -0,0 +1,36 @@
+package com.grupo3.bibliotecavirtual.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+
+    @Column(length = 300)
+    private String descripcion;
+
+    // Relación con Libro (Una categoría puede tener muchos libros)
+    @OneToMany(mappedBy = "categoria")
+    private List<Libro> libros;
+
+    // Constructor vacío obligatorio
+    public Categoria() {
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+}
